14e59fbaa9ebbd825149bdfe9fecf55cf5fbcdd9
Arjunan10 <81326478+Arjunan10@users.noreply.github.com>
2021-06-10 08:08:22 +0530

    Add files via upload

---
+++ ShopList.java
@@ -0,0 +1,64 @@
+package arylist;
+
+import java.util.ArrayList;
+
+public class ShopList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ArrayList
+ 		
+		/*
+		 * ArrayList<String>Chips=new ArrayList<String>(); Chips.add("Lays");
+		 * Chips.add("Crax"); Chips.add("Kurkure"); Chips.add("Domitos");
+		 * Chips.add("Bingo");
+		 * 
+		 * Chips.remove("Kurkure"); Chips.set(2, "Cheetos");
+		 * 
+		 * for(String i : Chips) { System.out.println(i); }
+		 */
+	
+     //2-D ArrayList 
+     
+ 	//ArrayList
+     
+     ArrayList<ArrayList<String>>Snacks=new ArrayList();
+     
+     ArrayList<String>Biscuits=new ArrayList<String>();
+     Biscuits.add("Milk Bikis");
+     Biscuits.add("Gooday");
+     Biscuits.add("Oreo");
+     Biscuits.add("Bourbon");
+     Biscuits.add("Marie Gold");
+
+     ArrayList<String>Choclates=new ArrayList<String>();
+     Choclates.add("Dairy Milk");
+     Choclates.add("5 Star");
+     Choclates.add("Mars");
+     Choclates.add("Snickers");
+     Choclates.add("Milky Bar");
+     
+     ArrayList<String>Chips=new ArrayList<String>();
+     Chips.add("Lays");
+     Chips.add("Crax");
+     Chips.add("Kurkure");
+     Chips.add("Domitos");
+     Chips.add("Bingo");
+        
+    Snacks.add(Biscuits);
+    Snacks.add(Choclates);
+    Snacks.add(Chips);
+   
+  
+  // for(ArrayList<String> i : Snacks) {
+	//   System.out.println(i);
+	//}
+    System.out.println(Snacks);
+    System.out.println("This Week's Best Selling Choclate: "+Snacks.get(1).get(0));
+    System.out.println("This Week's Best Selling Biscuit :"+Snacks.get(0).get(1));
+    System.out.println("This Week's Best Selling Chips :"+Snacks.get(2).get(0));
+   }
+	}
+	
+
+
