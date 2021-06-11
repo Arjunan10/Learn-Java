780904ec6b91f3cdda3fcd6664ca99bc4854f3c5
Arjunan10 <81326478+Arjunan10@users.noreply.github.com>
2021-06-11 22:42:02 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,32 @@
+package com.circle;
+
+import java.util.Scanner;
+
+public class Circle {
+
+        Scanner scan = new Scanner(System.in);
+        final double pi = 3.14;
+        public void user() {
+            System.out.println("-----------*************--------------");
+            System.out.println("Say Radius,I will give your \"Circle\"");
+            System.out.println("-----------*************--------------");
+            System.out.println();
+            System.out.print("Radius of your Circle: ");
+            double radius = scan.nextDouble();
+            double area = pi*(radius*radius);    //Area of Circle formula
+            System.out.println();
+            System.out.println("Area of Circle: "+area+" cm2");
+            double diameter = radius*2;
+            System.out.println();
+            System.out.println("Diameter of Circle: "+(int)diameter+" cm");//Diameter of  circle  formula
+            System.out.println();
+            double circumference = 2*pi*radius; //Circumference of Circle formula
+            System.out.println("Circumference of Circle: "+(int)circumference+" cm");
+        }
+
+        public static void main(String[] args) {
+            com.circle.Circle circle = new com.circle.Circle();
+            circle.user();
+        }
+    }
+
