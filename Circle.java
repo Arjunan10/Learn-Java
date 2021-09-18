c39f6442232af2895a08e629b92ebc3fb4f114ab
Arjun Ak <81326478+Arjunan10@users.noreply.github.com>
2021-09-19 00:05:38 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -16,16 +16,16 @@ public class Circle {
             double area = pi*(radius*radius);    //Area of Circle formula
             System.out.println();
             System.out.println("Area of Circle: "+area+" cm2");
-            double diameter = radius*2;
+            double diameter = radius*2;//Diameter of  circle  formula
             System.out.println();
-            System.out.println("Diameter of Circle: "+(int)diameter+" cm");//Diameter of  circle  formula
+            System.out.println("Diameter of Circle: "+(int)diameter+" cm");
             System.out.println();
             double circumference = 2*pi*radius; //Circumference of Circle formula
             System.out.println("Circumference of Circle: "+(int)circumference+" cm");
         }
 
         public static void main(String[] args) {
-            com.circle.Circle circle = new com.circle.Circle();
+            Circle circle = new Circle();
             circle.user();
         }
     }
